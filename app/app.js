2ad036228a4ce759390bab5973d8a0298d252260
Aleks <aadamovic@tehnik-design.com>
2018-03-12 22:55:04 -0400

    refac(app): remove version check
    no longer needed

--- app/app.js
+++ app/app.js
@@ -11,40 +11,7 @@ require([
 ) {
   domReady(function () {
 
-    if (!util.isOnline()) {
-      ko.applyBindings(new appViewModel());
-      return;
-    }
-
-    document.getElementById('refreshButton').onclick = function () {
-      location.reload(true);
-    }
-
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function () {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-        var repoData = JSON.parse(xmlHttp.responseText);
-
-        var lastCommitHash = localStorage['commitHash'];
-
-        //to make sure it exists and that things are up to date
-        //if someone has version before hash check
-        if (!lastCommitHash) {
-          localStorage['commitHash'] = repoData.object.sha;
-          location.reload(true);
-        }
-
-        if (lastCommitHash !== repoData.object.sha) {
-          document.getElementById('refresh').style.display = 'block';
-        }
-
-        localStorage['commitHash'] = repoData.object.sha;
-        ko.applyBindings(new appViewModel());
-      }
-    }
-
-    xmlHttp.open("GET", 'https://api.github.com/repos/lumenaut-network/stellarhq/git/refs/heads/master', true);
-    xmlHttp.send(null);
+    ko.applyBindings(new appViewModel());
   });
 });
 
